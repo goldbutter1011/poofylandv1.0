b6c4a0e6a31738b922cce04fafb93ff241bc7d7b
luckybutter <133196673+luckybutter322982@users.noreply.github.com>
2023-07-11 00:09:09 +1000

    flipdown change
    Signed-off-by: luckybutter <133196673+luckybutter322982@users.noreply.github.com>

---
+++ components/FlipDown.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { FlipDown, pad, appendChildren } from '../public/js/flipdown';
+import '../public/css/flipdown.css';
+
+const NextFlipdown = ({ endTime, theme = "dark", headings = ["Days", "Hours", "Minutes", "Seconds"] }) => {
+
+    const [containerId, setContainerId] = useState(`Flipdown-${[...Array(10)].map(() => Math.random().toString(36)[2]).join('')}`)
+
+    useEffect(() => {
+        let container = document.getElementById(containerId)
+        // create element with id=element_id class=flipdown
+        let element_id = [...Array(10)].map(() => Math.random().toString(36)[2]).join('')
+        let el = document.createElement('div')
+        el.id = element_id
+        el.className = 'flipdown'
+        // clear container
+        container.innerHTML = ''
+        // append element to container
+        container.appendChild(el)
+        // init flipdown
+        new FlipDown(
+            endTime,
+            element_id,
+            {
+                theme: theme,
+                headings: headings
+            }
+        ).start()
+    }, [endTime, theme, headings])
+
+    return (
+        <div id={containerId}></div>
+    )
+}
+
+export default NextFlipdown;
--- components/UpdateSection.tsx
+++ components/UpdateSection.tsx
@@ -1,7 +1,5 @@
 "use client"; // this is a client component
 import React from "react";
-import Link from "next/link";
-import { wrap } from "module";
 
 const Updatesection = () => {
   return (
